44c7553f52c58f543e4a567d1b835290ea34f86a
mithen.ji <jihonghuan20@gmail.com>
2024-11-28 10:29:21 +0800

    u config

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "mithenji.me"
-  port = String.to_integer(System.get_env("PORT") || "8080")
+  port = String.to_integer(System.get_env("PORT") || "80")
 
   config :website, WebsiteWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
